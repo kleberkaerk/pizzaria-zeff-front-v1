75829df74581bd252c7fa976a5ea8be5d77e1ea1
kleber.kaerk <apenaskleber123@gmail.com>
2023-01-20 18:35:26 -0300

    feature: criará componente search-products

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
+import { SearchProductsComponent } from './search-products/search-products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchProductsComponent
+  ],
   imports: [
     CommonModule,
     SearchRoutingModule
