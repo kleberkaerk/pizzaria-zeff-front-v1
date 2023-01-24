bf695b0b0eefd85e119e9b12011deec42a8c0cd8
kleber.kaerk <apenaskleber123@gmail.com>
2023-01-24 15:16:20 -0300

    feature: implementar serviço shopping-cart em componente search-products

--- src/app/search/search-products/search-products.component.ts
+++ src/app/search/search-products/search-products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from 'src/app/shared/domain/product';
 import { ProductService } from 'src/app/shared/service/product.service';
+import { ShoppingCartService } from 'src/app/shared/service/shopping-cart.service';
 import { Page } from 'src/app/shared/util/page';
 
 @Component({
@@ -16,7 +17,10 @@ export class SearchProductsComponent implements OnInit {
   public availablePages: Array<number> = new Array();
   public currentPage = 0;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private shoppingCartService: ShoppingCartService
+  ) { }
 
   private initializeAvailablePages(numberOfPages: number) {
 
@@ -42,15 +46,35 @@ export class SearchProductsComponent implements OnInit {
   ngOnInit(): void {
 
     this.searchProducts(this.valueOfSearch, 20, this.currentPage);
-
   }
 
-  public changePage(nextPage: number) {
+  private preventDefaultTouchStart(e: Event) {
+
+    if (e.cancelable && e.type === "touchstart") {
+
+      e.preventDefault();
+    }
+  }
+
+  public changePage(e: Event, nextPage: number) {
+
+    e.stopPropagation();
+
+    this.preventDefaultTouchStart(e);
 
     this.currentPage = --nextPage;
 
     this.searchProducts(this.valueOfSearch, 20, this.currentPage);
 
     window.scrollTo(0, 0);
+  }
+
+  public addProductToCard(e: Event, product: Product) {
+
+    e.stopPropagation();
+
+    this.preventDefaultTouchStart(e);
+
+    this.shoppingCartService.addProduct(product);
   }
 }
