3f2a8f3020cc6273e44fe9b514e44bae1178d418
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-14 14:34:20 -0300

    feature: ligar componente address com serviço address

--- src/app/address/addresses/addresses.component.ts
+++ src/app/address/addresses/addresses.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Address } from 'src/app/domain/address';
+import { AddressRequisitionService } from 'src/app/service/address-requisition.service';
 
 @Component({
   selector: 'app-addresses',
   templateUrl: './addresses.component.html',
   styleUrls: ['./addresses.component.css']
 })
-export class AddressesComponent {
+export class AddressesComponent implements OnInit {
 
+  public addresses = new Array<Address>();
+
+  constructor(
+    private addressRequisitionService: AddressRequisitionService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.addressRequisitionService.findUserAddress()
+      .subscribe(addresses => {
+
+        this.addresses = addresses;
+      });
+  }
 }
