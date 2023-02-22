c9e985d914f8ff69a9807cdec4d9d2eca1cefe34
kleber.kaerk <apenaskleber123@gmail.com>
2023-02-21 21:37:57 -0300

    refactor: desativar serviços que não estão sendo utilizados no momento

--- src/app/service/address-requisition.service.ts
+++ src/app/service/address-requisition.service.ts
@@ -1,54 +1,54 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+// import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Address } from '../domain/address';
-import { AddressDTO } from '../dto/address-dto';
-import { fromAddressDTOToAddress } from '../util/mapper';
-import { UserSessionService } from './user-session.service';
+// import { map, Observable } from 'rxjs';
+// import { environment } from 'src/environments/environment';
+// import { Address } from '../domain/address';
+// import { AddressDTO } from '../dto/address-dto';
+// import { fromAddressDTOToAddress } from '../util/mapper';
+// import { UserSessionService } from './user-session.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressRequisitionService {
 
-  private readonly urlBase = environment.urlBase;
-  private headers!: HttpHeaders;
+  // private readonly urlBase = environment.urlBase;
+  // private headers!: HttpHeaders;
 
-  constructor(
-    private httpClient: HttpClient,
-    private userSessionService: UserSessionService
-  ) { }
+  // constructor(
+  //   private httpClient: HttpClient,
+  //   private userSessionService: UserSessionService
+  // ) { }
 
-  public findUserAddress(): Observable<Array<Address>> {
+  // public findUserAddress(): Observable<Array<Address>> {
 
-    this.headers = new HttpHeaders({
-      "Authorization": "Basic " + this.userSessionService.getUserSession()
-    });
+  //   this.headers = new HttpHeaders({
+  //     "Authorization": "Basic " + this.userSessionService.getUserSession()
+  //   });
 
-    return this.httpClient.get<Array<AddressDTO>>(this.urlBase + "addresses/find-by-user", { headers: this.headers })
-      .pipe(map(addressesDTO => {
+  //   return this.httpClient.get<Array<AddressDTO>>(this.urlBase + "addresses/find-by-user", { headers: this.headers })
+  //     .pipe(map(addressesDTO => {
 
-        return addressesDTO.map(address => fromAddressDTOToAddress(address));
-      }))
-  }
+  //       return addressesDTO.map(address => fromAddressDTOToAddress(address));
+  //     }))
+  // }
 
-  private getCSRFToken(cookie: string): string {
+  // private getCSRFToken(cookie: string): string {
 
-    return cookie.split("XSRF-TOKEN=")[1].split(";")[0];
-  }
+  //   return cookie.split("XSRF-TOKEN=")[1].split(";")[0];
+  // }
 
-  public deleteAddress(addressId: number): Observable<HttpResponse<void>> {
+  // public deleteAddress(addressId: number): Observable<HttpResponse<void>> {
 
-    const csrfToken = this.getCSRFToken(document.cookie);
+  //   const csrfToken = this.getCSRFToken(document.cookie);
 
-    this.headers = new HttpHeaders({
-      "Authorization": "Basic " + this.userSessionService.getUserSession(),
-      "X-XSRF-TOKEN": csrfToken
-    });
+  //   this.headers = new HttpHeaders({
+  //     "Authorization": "Basic " + this.userSessionService.getUserSession(),
+  //     "X-XSRF-TOKEN": csrfToken
+  //   });
 
-    document.cookie = "XSRF-TOKEN=" + csrfToken;
+  //   document.cookie = "XSRF-TOKEN=" + csrfToken;
 
-    return this.httpClient.delete<void>(this.urlBase + "addresses/" + addressId.toString(), { observe: "response", headers: this.headers, withCredentials: true });
-  }
+  //   return this.httpClient.delete<void>(this.urlBase + "addresses/" + addressId.toString(), { observe: "response", headers: this.headers, withCredentials: true });
+  // }
 }
--- src/app/service/user-requisition.service.ts
+++ src/app/service/user-requisition.service.ts
@@ -1,51 +1,51 @@
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+// import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
-import { environment } from 'src/environments/environment';
-import { User } from '../domain/user';
+// import { environment } from 'src/environments/environment';
+// import { User } from '../domain/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRequisitionService {
 
-  private readonly urlBase = environment.urlBase;
+  // private readonly urlBase = environment.urlBase;
 
-  private headers!: HttpHeaders;
+  // private headers!: HttpHeaders;
 
-  constructor(private httpClient: HttpClient) { }
+  // constructor(private httpClient: HttpClient) { }
 
-  private encodeCredentials(username: string, password: string): string {
+  // private encodeCredentials(username: string, password: string): string {
 
-    return window.btoa(username + ":" + password);
-  }
+  //   return window.btoa(username + ":" + password);
+  // }
 
-  public signIn(username: string, password: string): Observable<HttpResponse<void>> {
+  // public signIn(username: string, password: string): Observable<HttpResponse<void>> {
 
-    this.headers = new HttpHeaders({
-      "Authorization": "Basic " + this.encodeCredentials(username, password)
-    });
+  //   this.headers = new HttpHeaders({
+  //     "Authorization": "Basic " + this.encodeCredentials(username, password)
+  //   });
 
-    return this.httpClient.get<void>(this.urlBase + "users/auth", { observe: "response", headers: this.headers });
-  }
+  //   return this.httpClient.get<void>(this.urlBase + "users/auth", { observe: "response", headers: this.headers });
+  // }
 
-  private getCSRFToken(cookie: string): string {
+  // private getCSRFToken(cookie: string): string {
 
-    return cookie.split("XSRF-TOKEN=")[1].split(";")[0];
-  }
+  //   return cookie.split("XSRF-TOKEN=")[1].split(";")[0];
+  // }
 
-  public signUp(user: User): Observable<HttpResponse<any>> {
+  // public signUp(user: User): Observable<HttpResponse<any>> {
 
-    const csrfToken = this.getCSRFToken(document.cookie);
+  //   const csrfToken = this.getCSRFToken(document.cookie);
 
-    this.headers = new HttpHeaders({
-      "Accept-Language": "pt-BR",
-      "X-XSRF-TOKEN": csrfToken
-    });
+  //   this.headers = new HttpHeaders({
+  //     "Accept-Language": "pt-BR",
+  //     "X-XSRF-TOKEN": csrfToken
+  //   });
 
-    document.cookie = "XSRF-TOKEN=" + csrfToken;
+  //   document.cookie = "XSRF-TOKEN=" + csrfToken;
 
-    return this.httpClient.put<any>(this.urlBase + "users/register", user, { observe: "response", headers: this.headers, withCredentials: true });
-  }
+  //   return this.httpClient.put<any>(this.urlBase + "users/register", user, { observe: "response", headers: this.headers, withCredentials: true });
+  // }
 }
